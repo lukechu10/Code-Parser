6dfc0822c5a5e15f69e3692a52cdf02ede2328da
Luke Chu <lukechu10@gmail.com>
2020-07-01 02:58:59 +0000

    test: simplify test with Evaluate() function

--- Tests/Test.cs
+++ Tests/Test.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 using System.IO;
 using Interpreter;
@@ -11,6 +12,19 @@ namespace Tests
 {
 	public class E2E
 	{
+		private void Evaluate(string testString, object expectedResult)
+		{
+			var tokenStream = new TokenStream(testString);
+			var parser = new Parser(tokenStream);
+			var evaluator = new Evaluator();
+
+			Statement expression = parser.Handle();
+
+			object result = evaluator.EvaluateStatement(expression);
+
+			Assert.Equal(expectedResult, result);
+		}
+
 		[Theory]
 		// binary operators
 		[InlineData("1 + 1;", 2)]
@@ -24,15 +38,7 @@ namespace Tests
 		[InlineData("1 > 0;", 1)]
 		public void Expression(string testString, double expectedResult)
 		{
-			var tokenStream = new TokenStream(testString);
-			var parser = new Parser(tokenStream);
-			var evaluator = new Evaluator();
-
-			Statement expression = parser.Handle();
-
-			object value = evaluator.EvaluateStatement(expression);
-
-			Assert.Equal(expectedResult, value);
+			this.Evaluate(testString, expectedResult);
 		}
 
 		[Theory]
@@ -41,15 +47,33 @@ namespace Tests
 		[InlineData("let x = 1 + 1;", null)]
 		public void Variables(string testString, object expectedResult)
 		{
-			var tokenStream = new TokenStream(testString);
-			var parser = new Parser(tokenStream);
-			var evaluator = new Evaluator();
-
-			Statement expression = parser.Handle();
-
-			object value = evaluator.EvaluateStatement(expression);
-
-			Assert.Equal(expectedResult, value);
+			this.Evaluate(testString, expectedResult);
 		}
+
+		[Theory]
+		[MemberData(nameof(VariableAssignmentData))]
+		public void VariableAssignment((string, object)[] statements)
+		{
+			var evaluator = new Evaluator();
+			foreach ((string, object) statement in statements)
+			{
+				var tokenStream = new TokenStream(statement.Item1);
+				var parser = new Parser(tokenStream);
+
+				Statement expression = parser.Handle();
+
+				object result = evaluator.EvaluateStatement(expression);
+
+				Assert.Equal(statement.Item2, result);
+			}
+		}
+
+		public static IEnumerable<object[]> VariableAssignmentData =>
+			new List<object[]>
+			{
+				new object[] {new[]{("let x = 1", (object)null), ("x", 1.0)}},
+				new object[] {new[]{("let x = 1", (object)null), ("x = 2", 2.0), ("x = x + x", 4.0)}},
+				new object[] {new[]{("let x = 1", (object)null), ("let y = 2", (object)null), ("x + y", 3.0)}}
+			};
 	}
 }
